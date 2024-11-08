cd1f31ab8bcb4a2bee37d1d390a4ae8f9d20c923
kk-developer92 <kk-developer92@gmail.com>
2024-11-08 18:47:23 +0500

    Firebase added get stream method

--- lib/core/api/firebase_api_client.dart
+++ lib/core/api/firebase_api_client.dart
@@ -31,10 +31,14 @@ class FirebaseFilterEntity {
 abstract class FirebaseApiClient {
   Future<dynamic> get(String collection, String id);
   Future<dynamic> delete(String collection, String id);
-  Future<dynamic> getAll(String collection,
-      {List<FirebaseFilterEntity>? filters});
+  Future<dynamic> getAll(
+    String collection, {
+    List<FirebaseFilterEntity>? filters,
+  });
   Future<dynamic> post(String collection, Map<String, dynamic> data);
   Future<dynamic> update(String collection, Map<String, dynamic> data);
+  Stream<QuerySnapshot<Map<String, dynamic>>> getStream(String collection,
+      {List<FirebaseFilterEntity>? filters});
 }
 
 class FirebaseApiClientImpl extends FirebaseApiClient {
@@ -125,5 +129,57 @@ class FirebaseApiClientImpl extends FirebaseApiClient {
   @override
   Future delete(String collection, String id) async {
     await firestore.collection(collection).doc(id).delete();
+  }
+
+  @override
+  Stream<QuerySnapshot<Map<String, dynamic>>> getStream(String collection,
+      {List<FirebaseFilterEntity>? filters}) {
+    Query query = firestore.collection(collection);
+
+    if (filters != null) {
+      for (var filter in filters) {
+        switch (filter.operator) {
+          case FirebaseApiFilterType.isEqualTo:
+            query = query.where(filter.field, isEqualTo: filter.value);
+            break;
+          case FirebaseApiFilterType.isNotEqualTo:
+            query = query.where(filter.field, isNotEqualTo: filter.value);
+            break;
+          case FirebaseApiFilterType.isGreaterThan:
+            query = query.where(filter.field, isGreaterThan: filter.value);
+            break;
+          case FirebaseApiFilterType.isGreaterThanOrEqualTo:
+            query =
+                query.where(filter.field, isGreaterThanOrEqualTo: filter.value);
+            break;
+          case FirebaseApiFilterType.isLessThan:
+            query = query.where(filter.field, isLessThan: filter.value);
+            break;
+          case FirebaseApiFilterType.isLessThanOrEqualTo:
+            query =
+                query.where(filter.field, isLessThanOrEqualTo: filter.value);
+            break;
+          case FirebaseApiFilterType.arrayContains:
+            query = query.where(filter.field, arrayContains: filter.value);
+            break;
+          case FirebaseApiFilterType.arrayContainsAny:
+            query = query.where(filter.field, arrayContainsAny: filter.value);
+            break;
+          case FirebaseApiFilterType.whereIn:
+            query = query.where(filter.field, whereIn: filter.value);
+            break;
+          case FirebaseApiFilterType.whereNotIn:
+            query = query.where(filter.field, whereNotIn: filter.value);
+            break;
+          case FirebaseApiFilterType.isNull:
+            query = query.where(filter.field, isNull: filter.value);
+            break;
+          default:
+            throw ArgumentError('Unsupported operator: ${filter.operator}');
+        }
+      }
+    }
+
+    return query.snapshots() as Stream<QuerySnapshot<Map<String, dynamic>>>;
   }
 }
